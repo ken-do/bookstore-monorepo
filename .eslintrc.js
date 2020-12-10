c3bbea91c09a5cdba99de12eccb29f005a5f4305
Khuong Do <khuongdo@nvg.vn>
2020-12-10 09:31:33 +0700

    chore(eslint): add more rules to eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
         sourceType: 'module',
+        project: ['./services/*/tsconfig.json'],
     },
     plugins: ['@typescript-eslint'],
     extends: [
@@ -13,9 +14,12 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:cypress/recommended',
-        // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:promise/recommended',
     ],
     rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-unused-vars': [
             'error',
             // we are only using this rule to check for unused arguments since TS
@@ -24,6 +28,56 @@ module.exports = {
         ],
         // most of the codebase are expected to be env agnostic
         'no-restricted-globals': ['error', ...DOMGlobals, ...NodeGlobals],
+        // TypesScript rules
+        '@typescript-eslint/no-unnecessary-type-assertion': 'off',
+        '@typescript-eslint/array-type': 'error',
+        '@typescript-eslint/consistent-type-assertions': 'error',
+        '@typescript-eslint/member-delimiter-style': [
+            'error',
+            {
+                multiline: {
+                    delimiter: 'none',
+                    requireLast: false,
+                },
+                singleline: {
+                    requireLast: false,
+                },
+            },
+        ],
+        '@typescript-eslint/member-ordering': 'error',
+        '@typescript-eslint/method-signature-style': 'error',
+        '@typescript-eslint/no-base-to-string': 'error',
+        '@typescript-eslint/no-extraneous-class': 'error',
+        '@typescript-eslint/no-invalid-void-type': 'error',
+        '@typescript-eslint/no-require-imports': 'error',
+        '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+        '@typescript-eslint/no-unnecessary-qualifier': 'error',
+        '@typescript-eslint/no-unnecessary-type-arguments': 'error',
+        '@typescript-eslint/prefer-for-of': 'error',
+        '@typescript-eslint/prefer-includes': 'error',
+        '@typescript-eslint/prefer-nullish-coalescing': 'error',
+        '@typescript-eslint/prefer-optional-chain': 'error',
+        '@typescript-eslint/prefer-reduce-type-parameter': 'error',
+        '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+        '@typescript-eslint/promise-function-async': 'error',
+        '@typescript-eslint/switch-exhaustiveness-check': 'error',
+        '@typescript-eslint/type-annotation-spacing': 'error',
+        '@typescript-eslint/unified-signatures': 'error',
+        // Promise rules
+        'promise/catch-or-return': 'error',
+        'promise/no-return-wrap': 'error',
+        'promise/param-names': 'error',
+        'promise/always-return': 'off',
+        'promise/no-native': 'off',
+        'promise/no-nesting': 'error',
+        'promise/no-promise-in-callback': 'error',
+        'promise/no-callback-in-promise': 'error',
+        'promise/no-new-statics': 'error',
+        'promise/no-return-in-finally': 'error',
+        'promise/valid-params': 'error',
+        'promise/valid-params': 'error',
+        'promise/prefer-await-to-then': 'error',
+        'promise/prefer-await-to-callbacks': 'error',
     },
     overrides: [
         // tests, no restrictions (runs in Node / jest with jsdom)
